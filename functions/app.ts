f8d06e31dcaee089a2b8a256b65df8d845f38cd2
Akiacode <catry.me@gmail.com>
2022-02-17 18:13:53 +0900

    rewrite

--- functions/app.ts
+++ functions/app.ts
@@ -12,12 +12,26 @@ const worldList = async (): Promise<string[]> => await wordnet.list().filter(wor
 export default async function (instance: FastifyInstance, _opts: FastifyServerOptions, done) {
 
     instance.get('/words', async (_req: FastifyRequest, res: FastifyReply) => {
-        res.status(200).send({ wordList: await worldList() }).setCookie('dictry', 'serverless', { maxAge: 8000000 })
+        res.status(200).send({ wordList: await worldList() })
     })
 
     instance.get('/words/:name', async (req: FastifyRequest<Word>, res: FastifyReply) => {
         const { name } = req.params
         await wordnet.lookup(name).then((definitions: any) => res.status(200).send({ glossary: definitions[0].glossary })).catch((_: unknown) => res.status(200).send({ glossary: 'No definition found' }))
+    })
+
+    instance.get('/word/today', async (req: FastifyRequest, res: FastifyReply) => {
+        if (req.headers.date === undefined) return res.status(404).send({ error: 'No date header' })
+        const wordList = await worldList()
+
+        // https://github.com/cwackerfuss/react-wordle/blob/main/src/lib/words.ts#L56~L60
+        const epochMs = new Date('January 1, 2022 00:00:00').valueOf()
+        const now = req.headers.date
+        const msInDay = 86400000
+        const index = Math.floor((Number(now) - epochMs) / msInDay)
+        const nextday = (index + 1) * msInDay + epochMs
+
+        res.status(200).send({ word: wordList[index % wordList.length], nextday: nextday })
     })
 
     done()
